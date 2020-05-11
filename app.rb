b1541b08e9d2da2f4bb1906091b6bc30809caac3
epittab <ep7m@me.com>
2020-05-11 14:16:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        name = 'Enrique'
+        "My name is #{name}"
+    end
+
+    get '/hometown' do 
+        town = 'Winchester'
+        "My hometown is #{town}"
+    end
+
+    get '/favorite-song' do 
+        song = 'No se tu'
+        "My favorite song is #{song}"
+    end
+
 end
